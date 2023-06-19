40cda7c7212f8358055987e03f06ca376dabcaf8
PieroJacinto <pierojacinto@gmail.com>
2023-06-19 19:46:49 -0300

    ultimos retoques a la aplicacion

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import { AddCategory, GifGrid } from "./components";
+
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(["One Punch"]);
@@ -10,7 +10,7 @@ export const GifExpertApp = () => {
         if ( categories.includes( newCategory ) ) return;  
         
         setCategories([ newCategory, ...categories ])
-        // setCategories( cat => [ "Valorant", ...cat ] )
+        
     }
     
   return (
--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,27 +1,17 @@
-import { useEffect, useState } from "react";
-
 import { GifItem } from "./GifItem";
-import { getGifs } from "./GetGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ( { category }) => {   
-    
-    const [images, setimages] = useState([]);
 
-    const getImages = async() => {
-        const newImages = await
-        getGifs( category );
-        setimages( newImages)
-    }
-
-    useEffect( () => {
-        getImages();
-    }, [ ])
-
+    const { images, isLoading } = useFetchGifs( category );
 
     return (
         <>
             <h3>{ category }</h3>
-            
+            {
+                isLoading && ( <h2>Cargando...</h2> )
+            }
+
             <div className="card-grid">
             { 
                 images.map( ( image ) => (                
